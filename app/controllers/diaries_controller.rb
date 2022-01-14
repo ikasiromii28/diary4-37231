9a8075702590f83437483094111aa6f9753b4183
ikasiromii28 <camelliakamesan716@gmail.com>
2022-01-14 09:58:35 +0900

    雛形作成

---
+++ app/controllers/diaries_controller.rb
@@ -0,0 +1,40 @@
+class DiariesController < ApplicationController
+  before_action :set_diary, only: [:edit, :show]
+
+  def index
+    @diaries = Diary.all
+  end
+
+  def new
+    @diary = Diary.new
+  end
+
+  def create
+    Diary.create(diary_params)
+  end
+
+  def destroy
+    diary = Diary.find(params[:id])
+    diary.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    diary = Diary.find(params[:id])
+    diary.update(diary_params)
+  end
+
+  def show
+  end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:name, :image, :text)
+  end
+
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
+end
